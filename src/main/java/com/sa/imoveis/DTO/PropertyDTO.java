99780f0c7590d2a410786bdd6eea12e5eb7c1c64
João Vitor <jvitorcavalcante18@gmail.com>
2025-07-08 11:27:15 -0300

    feat: Criando a camada de DTOs

---
+++ src/main/java/com/sa/imoveis/DTO/PropertyDTO.java
@@ -0,0 +1,13 @@
+package com.sa.imoveis.DTO;
+
+public class PropertyDTO {
+    private String address;
+    private int numberOfRooms;
+    private int numberOfBathrooms;
+    private int numberOfPossibleCars;
+    private double landSize;
+    private int addressNumber;
+    private Long consultantID;
+    private Double salePrice;
+    private Double rentalPrice;
+}
---
+++ src/main/java/com/sa/imoveis/DTO/UserDTO.java
@@ -0,0 +1,8 @@
+package com.sa.imoveis.DTO;
+
+public class UserDTO {
+    private String name;
+    private String email;
+    private String password;
+    private String cellphone;
+}
