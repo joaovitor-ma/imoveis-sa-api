8be3156cb8dd20b1761088af8fdde7af33238f19
João Vitor <jvitorcavalcante18@gmail.com>
2025-07-08 11:50:47 -0300

    fix: Adicionando getters e setters das classes UserDTO e PropertyDTO

--- src/main/java/com/sa/imoveis/dto/UserDTO.java
+++ src/main/java/com/sa/imoveis/dto/UserDTO.java
@@ -5,4 +5,36 @@ public class UserDTO {
     private String email;
     private String password;
     private String cellphone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
 }
